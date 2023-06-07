8b909bc1d27ab9145f461d2f1a9d6c2a5519dd49
Ricardo Rojas <rojasricor@gmail.com>
2023-06-07 15:52:49 -0500

    ok

--- env.php
+++ env.php
@@ -2,4 +2,4 @@
 BASE_DE_DATOS_MYSQL = 'u361342399_spaceitfip_db'
 USUARIO_MYSQL       = 'root'
 CLAVE_MYSQL         = ''
-SERVIDOR_MYSQL      = '127.0.0.1:3306'
+SERVIDOR_MYSQL      = '127.0.0.1:3308'
